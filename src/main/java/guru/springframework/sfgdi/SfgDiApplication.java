9a0289bb9360827caf6b87f10673ac2494e489f9
Seppe Van Laer <25950796+svlaer@users.noreply.github.com>
2020-09-22 16:47:20 +0200

    Designate primary Bean

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -15,8 +15,8 @@ public class SfgDiApplication {
 		ConfigurableApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
 		MyController myController = (MyController) ctx.getBean("myController");
-		String greeting = myController.sayHello();
-		System.out.println(greeting);
+		System.out.println("----- Primary Bean");
+		System.out.println(myController.sayHello());
 
 		System.out.println("----- Property");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
