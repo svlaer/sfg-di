6d29808b98c7a95a2994f9d745e695d0e524a6fe
Seppe Van Laer <25950796+svlaer@users.noreply.github.com>
2020-09-22 16:42:12 +0200

    Use qualifiers to specify which beans to inject

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -15,27 +15,19 @@ public class SfgDiApplication {
 		ConfigurableApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
 		MyController myController = (MyController) ctx.getBean("myController");
-
 		String greeting = myController.sayHello();
-
 		System.out.println(greeting);
 
 		System.out.println("----- Property");
-
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-
 		System.out.println(propertyInjectedController.getGreeting());
 
 		System.out.println("----- Setter");
-
 		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-
 		System.out.println(setterInjectedController.getGreeting());
 
 		System.out.println("----- Constructor");
-
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-
 		System.out.println(constructorInjectedController.getGreeting());
 	}
 
